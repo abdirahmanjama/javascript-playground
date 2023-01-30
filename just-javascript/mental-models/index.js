7d112f8878407b48fe000589ac281f6e514bf260
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-30 20:51:40 +0000

    add duplicateSpreadsheet function

--- just-javascript/mental-models/index.js
+++ just-javascript/mental-models/index.js
@@ -6,6 +6,7 @@
  * In this document, we will identify and look at snippets of code. 
  * The main idea is to ensure, we can reflect on our mental model. 
  * 
+ * In this file, we will create and look at two different functions. Let's try to see if we actually understand what's happening. 
  * 
  */
 
@@ -18,6 +19,7 @@ function exampleOne() {
 }
 
 /**
+ * 
  * How does exampleOne actually work? What is your mental Model
  * 
  * let a = 10; -> declare a variable called a and set it to the value 10. 
@@ -25,4 +27,32 @@ function exampleOne() {
  * a = 0; -> set a to 0. 
  * 
  * After following line by line, we can see that a = 10 and b = 0;
+ *
+ */
+
+
+/**
+ * Thinking fast and slow
+ * 
+ * When coding/thinking we usually two thinking models. 
+ */
+
+function duplicateSpreadsheet(original) {
+    if (original.hasPendingChanges) {
+        throw new Error('Please save your file before duplicating it')
+    }
+
+    let copy = {
+        created: Date.now(),
+        author: original.author,
+        cells: original.cells,
+        metadata: original.metadata
+    }
+
+    copy.metadata.title = 'Copy of ' + original.metadata.title
+    return copy;
+}
+
+/**
+ * Slow
  */
