46f34571b2a65a87244af9f4cc25f2f3ed8a9e43
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-31 23:40:07 +0000

    playaround

---
+++ just-javascript/the-javascript-universe/index.js
@@ -0,0 +1,13 @@
+console.log(2);
+console.log("hello");
+console.log(undefined);
+
+console.log(typeof ([1, 2, 3].map(x => x % 2 == 0)))
+
+
+function validDate(date) {
+    if (typeof date === 'date') {
+        return 'Valid date'
+    }
+    throw Error('Invalid date')
+}
